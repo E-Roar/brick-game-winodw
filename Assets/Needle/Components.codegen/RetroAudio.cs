4095f258e046c64c1c231baf12310e43c222e6c0
E-Roar <er@e-roar.dev>
2026-07-04 18:54:48 +0100

    fix: restructure git repository to track Unity project

---
+++ Assets/Needle/Components.codegen/RetroAudio.cs
@@ -0,0 +1,19 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class RetroAudio : UnityEngine.MonoBehaviour
+	{
+		public void init() {}
+		public void playMove() {}
+		public void playDrop() {}
+		public void playRotate() {}
+		public void playClear() {}
+		public void playGameOver() {}
+	}
+}
+
+// NEEDLE_CODEGEN_END
---
+++ Assets/Needle/Components.codegen/TetrisGame.cs
@@ -0,0 +1,20 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class TetrisGame : UnityEngine.MonoBehaviour
+	{
+		public void initAudio() {}
+		public void Start() {}
+		public void move(float @dir) {}
+		public void drop() {}
+		public void rotate() {}
+		public void Update(float @dt) {}
+		public void draw() {}
+	}
+}
+
+// NEEDLE_CODEGEN_END
